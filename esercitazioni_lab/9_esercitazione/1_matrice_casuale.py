de7ce414fbae75c09f15cdeec4d66cc80769e344
imy <atyqiman@gmail.com>
2025-12-01 01:52:05 +0100

    Update 1_matrice_casuale.py

--- esercitazioni_lab/9_esercitazione/1_matrice_casuale.py
+++ esercitazioni_lab/9_esercitazione/1_matrice_casuale.py
@@ -1,16 +1,16 @@
 from random import randint
 
 def main():
-    w = int(input("w? "))
-    h = int(input("h? "))
+    w = int(input("w? ")) # n colonne
+    h = int(input("h? ")) # n righe
 
-    matrix = [[0 for _ in range(w)] for _ in range(h)]
+    matrix = [[0 for _ in range(w)] for _ in range(h)] # riga di zeri ripetuta per quante righe
 
     for i in range(w*h):
         y = randint(0, h-1)
         x = randint(0, w-1)
         
-        while matrix[y][x] != 0:
+        while matrix[y][x] != 0: # se pos generata è già piena rigenero
             y = randint(0, h-1)
             x = randint(0, w-1)
         
